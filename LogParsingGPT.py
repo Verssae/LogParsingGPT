333621c200051f126785589d57fe84307ae3c074
Hansae Ju <sparky@hanyang.ac.kr>
2023-05-18 15:30:34 +0900

    Parser done but dups not yet

--- LogParsingGPT.py
+++ LogParsingGPT.py
@@ -2,8 +2,11 @@ import random
 import openai
 import os
 import re
+from typing import Tuple
 
 from tqdm import tqdm
+
+# You are an AI log analysis expert. Given a log message by user, parse it into variables and template using python f-string syntax. If there are no semantic variable names in this log, just provide `template = log_message`. The code you provide should be excutable and can generate the same user's input log. Do not include any explanations. Only provide the code. Here are some examples:
 
 instruction = """
 You are an AI log analysis expert. You should look a log message given by user then generate python code including variable assigning lines and f-string template assigining line. If there are no semantic variable names in this log message, template should be same as the log message. The generated code should generate the user's input log. Followings are examples:
@@ -33,10 +36,10 @@ Third, you should update the template to make it more generic and meaningful. Th
 """.strip()
 
 class LogParsingGPT:
-    def __init__(self, openai_api_key: str = None) -> None:
+    def __init__(self, temparature=0.2, openai_api_key: str = None) -> None:
         openai.api_key = openai_api_key or os.environ.get('OPENAI_API_KEY')
         self.model = 'gpt-3.5-turbo'
-
+        self.temparature = temparature
         self.instruction = instruction
         self.examples = examples
         self.second_instruction = second_instruction
@@ -46,7 +49,7 @@ class LogParsingGPT:
     def llm_run(self, user_prompt: str) -> str:
         response = openai.ChatCompletion.create(
             model=self.model,
-            top_p=1,
+            temperature=self.temparature,
             messages=self.messages + [{"role": "user", "content": user_prompt}]
         )
         return response['choices'][0]['message']['content']
@@ -54,37 +57,22 @@ class LogParsingGPT:
     def output_parse(self,llm_output: str) -> dict:
         llm_output = llm_output.replace('ASSISTANT:\n', '')
         *variables, template = llm_output.split('\n')
-        try:
-            before = locals().copy()
-            exec('\n'.join(variables))
-            after = locals().copy()
-            variables = {k: after[k] for k in after if k not in before and k != 'before'}
-            
-        except Exception as e:
-            print("ERROR:",e)
-            print(llm_output)
-            exit(0)
 
-        try:
-            template = template.split('=')[1].strip()
-            # remove f-string
-            template = template[2:-1]
-            # remove all '\' from template
-            template = template.replace('\\', '')
-        except Exception as e:
-            print("ERROR:",e)
-            print(llm_output)
-            exit(0)
+        before = locals().copy()
+        exec('\n'.join(variables))
+        after = locals().copy()
+        variables = {k: after[k] for k in after if k not in before and k != 'before'}
+        
+        template = template.split('=')[1].strip()
+
+        if template.startswith('f'):
+            template = template[1:]
+        template = template.strip("'").strip('"').strip()
+
+        # re-esacpe
+        template = template.replace("\\'", "'")
 
         return {'variables': variables, 'template': template}
-    
-    def yes_or_no(self, llm_output: str) -> bool:
-        if llm_output.lower().startswith('yes'):
-            return True
-        elif llm_output.lower().startswith('no'):
-            return False
-        else:
-            raise ValueError('llm_output should start with yes or no')
         
 def check_substring_set(string, substring_set):
     current_index = 0
@@ -95,9 +83,12 @@ def check_substring_set(string, substring_set):
         current_index = substring_index + len(substring)
     return True
        
-def match_template(logs: list[str], template: str):
-    stared_template = var_to_star(template)
-    return [ log for log in logs if check_substring_set(log, stared_template.split('<*>'))]
+def match_template(logs: list[str], template: str) -> list[str]:
+    template = replace_variable(template)
+    template = re.escape(template)
+    template = template.replace(r'<\*>', r'(.+)')
+    regex = re.compile(template)
+    return [ log for log in logs if regex.match(log) ]
      
 def replace_variable(string):
     pattern = r'{(.*?)}'  # '{variable_name}' 패턴을 정규식으로 표현
@@ -108,42 +99,63 @@ def var_to_star(template: str) -> str:
     replaced_template = replace_variable(template)
     return replaced_template
 
-def run(logs: list[str]):
-    log_parser = LogParsingGPT()
-    result = {}
-    logset = set(logs)
+def pipeline(logset: set[str], result: dict[str, dict], temparature=0.2) -> Tuple[dict[str, dict], set[str]]:
+    print('---PIPELINE---')
+    log_parser = LogParsingGPT(temparature=temparature)
     all_matches = set()
-    total = len(logset)
     for unique_log in logset:
         if unique_log in all_matches:
             continue
-        print(f'Parsed {len(all_matches)}/{total} logs')
+        print(f'Parsed {len(all_matches)} / {len(logset)} logs')
         llm_output = log_parser.llm_run(f"'{unique_log}'")
-        output = log_parser.output_parse(llm_output)
+        try:
+            output = log_parser.output_parse(llm_output)
+        except Exception as e:
+            print("---[ERROR]---\n",e)
+            print(f'{unique_log} \n-> \n{llm_output}')
+            print("---[SKIP THIS LOG]---")
+            continue
         matches = match_template(list(logset), output['template'])
-                
-        result[output['template']] = {
-            'variables': output['variables'],
-            'matches': matches,
-        }
+
+        if len(matches) > 0:
+            result[output['template']] = {
+                'variables': output['variables'],
+                'matches': matches,
+            }
+        else:
+            print("---[NO MATCH]---")
+            print(f'{unique_log} \n-> \n{output["template"]}')
+            print("---[SKIP THIS LOG]---")
         all_matches.update(matches)
-        
-    print(f'Parsed {len(result)} templates from {total} logs')
-    for t in result:
-        print(t)
+    print(f'Parsed {len(result)} templates from {len(all_matches)} / {len(logset)} logs')
+    
     return result, logset - all_matches
 
+def run(logs: list[str]) -> dict[str, dict]:
+    result = {}
+    random.shuffle(logs)
+    logset = set(logs)
+    result, logset = pipeline(logset, result, temparature=0.0)
+    while len(logset) > 0:
+        result, logset = pipeline(logset, result, temparature=0.8)
+    return result
+
 def duplicate_template(templates: list[str]):
+    dups = {}
     for t in templates:
         result = match_template(templates, t)
         if len(result) > 1:
-            print(t)
-            print([ r for r in result if r != t])
-                
+            subs = [ r for r in result if r != t]
+            dups[t] = subs
+    same = []
+    for t in dups:
+        for s in dups[t]:
+            if t in dups[s]:
+                same.append((t,s))
+    return dups, same
+    
+            
 
-def revisit_template(logs: list[str], stared_template: str):
-    # 교집합이 있는 경우, 
-    return [ log for log in logs if check_substring_set(log, stared_template.split('<*>'))]
         
 if __name__ == '__main__':
     from data_utils import load_dataset
@@ -153,11 +165,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', type=str, required=True)
     parser.add_argument('--read', type=bool, default=False)
-    parser.add_argument('--auto', type=bool, default=False, help='No need to confirm')
     args = parser.parse_args()
 
     if args.read:
-        with open(f'result_{args.dataset}.json', 'r') as f:
+        with open(f'results/result_{args.dataset}.json', 'r') as f:
             result = json.load(f)
 
         duplicate_template(result.keys())
@@ -165,50 +176,12 @@ if __name__ == '__main__':
 
     test_data = load_dataset(args.dataset)
     logs = test_data['log'].tolist()
-    random.shuffle(logs)
-    result, logs = run(logs)
+    result = run(logs)
 
-    duplicate_template(result.keys())
+    # print('---[Duplicated templates]---')
+    # duplicate_template(result.keys())
 
-    # test_templates = test_data['template'].unique()
-
-    # if not args.auto:
-    #     confirm = input(f'You are going to parse {len(test_templates)} templates from {args.dataset}. Continue? (y/n) ')
-    #     if confirm.lower() != 'y':
-    #         exit(0)
-
-    # log_parser = LogParsingGPT()
-
-    # result = []
-
-    # for template in tqdm(test_templates):
-    #     log_messages = test_data[test_data['template'] == template]
-    #     log_messages = log_messages['log'].tolist()
-
-    #     sample_log = random.choice(log_messages)
-    #     print(f'Parsing [{template}] \nfrom [{sample_log}]')
-
-    #     llm_output = log_parser.llm_run(f"'{sample_log}'")
-    #     output = log_parser.output_parse(llm_output)
-
-    #     regex_template = sem_to_regex(output['template'], list(output['variables'].keys()))
-    #     matches = match_template(log_messages, regex_template)
-    #     output['oracle'] = template
-    #     output['sample_log'] = sample_log
-    #     output['accuracy'] = len(matches) / len(log_messages)
-    #     output['unmatched'] = [ log for log in log_messages if log not in matches ]
-    #     output['wrong_matches'] = [ log for log in matches if log not in log_messages ]
-    #     result.append(output)
-    #     print(f'Parsed [{output["template"]}] with Variables [{output["variables"]}]\nfrom [{sample_log}]')
-    #     print(regex_template)
-    #     print(f'Matches: {len(matches)} / {len(log_messages)}')
-    #     print('----------------------------------')
-    
-    # sve result to file
     import json
-    with open(f'result_{args.dataset}.json', 'w') as f:
+    with open(f'results/result_{args.dataset}.json', 'w') as f:
         json.dump(result, f, indent=4)
-
-    with open(f'left_{args.dataset}.txt', 'w') as f:
-        f.write('\n'.join(logs))
 
